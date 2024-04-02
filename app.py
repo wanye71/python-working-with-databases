6867c0d4db864a9bce8ea8ec6034d69dbe4e7428
wayne71 <wayne.hatter1971@gmail.com>
2024-04-01 21:19:42 -0500

    Insert Data into table

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ connection = sqlite3.connect('movies.db')
 
 cursor = connection.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS Movies 
-	(Title TEXT, Director TEXT, Year INT)''')
+# cursor.execute('''CREATE TABLE IF NOT EXISTS Movies
+# (Title TEXT, Director TEXT, Year INT)''')
+cursor.execute('''INSERT INTO Movies VALUES('Taxi Driver', 'Martin Scorsese', 1976)''')
+cursor.execute('''SELECT * FROM Movies''')
+
+print(cursor.fetchone())
 
 connection.commit()
 connection.close()
